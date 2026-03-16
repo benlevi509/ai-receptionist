133f43069cbfe893368e80ee6b56aa1f1126c6da
benlevi509 <benjilevi77@gmail.com>
2026-03-16 12:19:06 +0000

    Add restaurant information and booking details

---
+++ restaurant.js
@@ -0,0 +1,29 @@
+export const restaurant = {
+  name: "Benji's Restaurant",
+
+  address: "London, United Kingdom",
+
+  cuisine: "Modern European",
+
+  openingHours: {
+    monday: "12:00 - 22:00",
+    tuesday: "12:00 - 22:00",
+    wednesday: "12:00 - 22:00",
+    thursday: "12:00 - 22:00",
+    friday: "12:00 - 23:00",
+    saturday: "12:00 - 23:00",
+    sunday: "12:00 - 21:00"
+  },
+
+  booking: {
+    maxPartySize: 8,
+    slotDurationMinutes: 90,
+    maxBookingsPerSlot: 10
+  },
+
+  contact: {
+    phone: "+447402767133",
+    email: "benjilevi77@gmail.com"
+  }
+
+};
