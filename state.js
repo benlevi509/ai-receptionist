b4d3bece1cb87fe6ac7d6cdd98f350abc41fcdba
benlevi509 <benjilevi77@gmail.com>
2026-07-08 13:23:29 +0100

    Update state.js

--- state.js
+++ state.js
@@ -1,18 +1,22 @@
 export const state = {
   conversationHistory: [],
 
-  booking: {},
+  booking: {
+    people: null,
+    date: null,
+    time: null,
+    name: null
+  },
 
   bookingActive: false,
   bookingStep: null,
 
   pendingTime: null,
   pendingName: null,
-
   correctionTarget: null,
 
   silenceCount: 0,
-
+  repeatCount: 0,
   lastQuestion: null,
 
   awaitingAvailabilityConfirmation: false
@@ -21,18 +25,22 @@ export const state = {
 export function resetState() {
   state.conversationHistory = [];
 
-  state.booking = {};
+  state.booking = {
+    people: null,
+    date: null,
+    time: null,
+    name: null
+  };
 
   state.bookingActive = false;
   state.bookingStep = null;
 
   state.pendingTime = null;
   state.pendingName = null;
-
   state.correctionTarget = null;
 
   state.silenceCount = 0;
-
+  state.repeatCount = 0;
   state.lastQuestion = null;
 
   state.awaitingAvailabilityConfirmation = false;
