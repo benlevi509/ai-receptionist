e1cdd163affb6c327b26d7b6fc7358e6473f6afd
benlevi509 <benjilevi77@gmail.com>
2026-08-10 17:26:22 +0100

    Pass call metadata safely into realtime stream

--- twilio.js
+++ twilio.js
@@ -1,36 +1,27 @@
-import businessConfig from "./businessConfig.js";
-
 function escapeXml(text) {
   return String(text || "")
     .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/\"/g, "&quot;")
+    .replace(/"/g, "&quot;")
     .replace(/'/g, "&apos;");
 }
 
-export function mediaStreamResponse(host, callerNumber = "") {
+export function mediaStreamResponse(host, { callerNumber = "", callSid = "" } = {}) {
   const cleanHost = String(host || "")
-    .replace(/^https?:\/\//, "")
-    .replace(/^wss?:\/\//, "");
+    .replace(/^https?:\/\//i, "")
+    .replace(/^wss?:\/\//i, "")
+    .replace(/\/$/, "");
+
+  if (!cleanHost) throw new Error("Cannot create Twilio media stream without a public host.");
 
   return `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Connect>
     <Stream url="wss://${escapeXml(cleanHost)}/media-stream">
       <Parameter name="callerNumber" value="${escapeXml(callerNumber)}" />
+      <Parameter name="callSid" value="${escapeXml(callSid)}" />
     </Stream>
   </Connect>
 </Response>`;
 }
-
-// Kept as a simple fallback/debug helper while the realtime bridge is rolled out.
-export function sayAndGather(reply) {
-  return `<Response><Say voice="Polly.Brian" language="en-GB">${escapeXml(reply)}</Say></Response>`;
-}
-
-export function sayAndHangup(reply) {
-  return `<Response><Say voice="Polly.Brian" language="en-GB">${escapeXml(reply)}</Say><Hangup/></Response>`;
-}
-
-export { businessConfig };
